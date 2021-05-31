e92a12e02479fa63a9e89d1ae7cccd05c2a9011b
Thomas Begush <tbegush@gmail.com>
2021-05-30 19:59:36 -0700

    organized HTML a little better

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -20,19 +20,21 @@ function makeQuestion(){
     var question = allQuestions[qCounter].question;
     //paint the question on the <p> element
         questionParaEl.textContent = question;
-        questionAnswerEl
-    .append(questionParaEl);
+//stick the question, which is inside a <p>, into the QandA element
+     questionAnswerEl.append(questionParaEl);
+//take all the actual answers and throw them in a variable called answers
     var answers = allQuestions[qCounter].answers;
 
+    //Loop through each answer, put it in a button
     for (var i = 0; i < answers.length; i++){
     var buttonEl = document.createElement("button");
     buttonEl.textContent = answers[i];
     answersEl.append(buttonEl);
     }
-    
+    //Take all the buttons and paint them on the page
     questionAnswerEl.append(answersEl)
 
-    results.append(questionAnswerEl)
+    quiz.append(questionAnswerEl)
 
 }
 
@@ -50,11 +52,16 @@ var timerEl = document.createElement("timer")
 
 // function showResults(){
 
+//     if (selected answer = correctAnswer) {
+//         window.alert("You're right!")
+//         time += 10;
+//     }
 
 // }
 
 // display quiz on page load. 
 makeQuestion();
+startTimer();
 
 // on submit, show results
-submitButton.addEventListener('click', showResults);
+// submitButton.addEventListener('click', xxxxxxxxxxx);
